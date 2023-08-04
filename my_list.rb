b2de27a7e520eff7add8ae75cc91e70d79f3a269
Derrick1451 <haylederrick14@gmail.com>
2023-08-04 14:30:45 +0300

    Update my_list.rb file

--- my_list.rb
+++ my_list.rb
@@ -7,3 +7,11 @@ class MyList
     @list = args
   end
 end
+
+# add code -derrick 
+def each(&block)
+  @list.each { |item| block.call(item) }
+end
+def each(&block)
+  @list.each { |item| block.call(item) }
+end
